fa944b429fb52ca0dd8822687735fbd21c6605e8
Daniel <buaadaniel@163.com>
2022-06-29 01:53:51 +0800

    refactor: 重构RpcServer

--- src/main/java/com/buaadaniel/rpc/core/server/RpcServer.java
+++ src/main/java/com/buaadaniel/rpc/core/server/RpcServer.java
@@ -6,29 +6,32 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.concurrent.*;
 
+/**
+ * @author Daniel
+ */
 public class RpcServer {
+
     private final ExecutorService threadPool;
-    // interfaceName -> interfaceImplementation object
+
     private final HashMap<String, Object> registeredService;
 
     public RpcServer() {
-
         int corePoolSize = 5;
         int maximumPoolSize = 50;
         long keepAliveTime = 60;
         BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(100);
         ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        this.threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workingQueue, threadFactory);
-        this.registeredService = new HashMap<String, Object>();
+        this.threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS,
+                workingQueue, threadFactory);
+        this.registeredService = new HashMap<>();
     }
 
-    // 参数service就是interface的implementation object
     public void register(Object service) {
         registeredService.put(service.getClass().getInterfaces()[0].getName(), service);
     }
 
     public void serve(int port) {
-        try (ServerSocket serverSocket = new ServerSocket(port)){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("server starting...");
             Socket handleSocket;
             while ((handleSocket = serverSocket.accept()) != null) {
@@ -36,7 +39,7 @@ public class RpcServer {
                 threadPool.execute(new RpcServerWorker(handleSocket, registeredService));
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("ERROR happened on server side: " + e.getMessage());
         }
     }
 }
